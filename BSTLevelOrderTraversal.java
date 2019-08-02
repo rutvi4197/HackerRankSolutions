8b0d57674d4ba967de7ca2a19d7a632ced23b78a
Rutvi Shah <20351385+rutvi4197@users.noreply.github.com>
2019-08-02 13:06:51 +0530

    Create BSTLevelOrderTraversal.java

---
+++ BSTLevelOrderTraversal.java
@@ -0,0 +1,37 @@
+static void levelOrder(Node root){
+      //Write your code here
+        int h = height(root);
+        int i;
+        for (i=1; i<=h; i++)
+            printGivenLevel(root, i);
+    
+      
+    }
+ static int height(Node root)
+    {
+        if (root == null)
+           return 0;
+        else
+        {
+            /* compute  height of each subtree */
+            int lheight = height(root.left);
+            int rheight = height(root.right);
+             
+            /* use the larger one */
+            if (lheight > rheight)
+                return(lheight+1);
+            else return(rheight+1); 
+        }
+    }
+static void printGivenLevel (Node root ,int level)
+    {
+        if (root == null)
+            return;
+        if (level == 1)
+            System.out.print(root.data + " ");
+        else if (level > 1)
+        {
+            printGivenLevel(root.left, level-1);
+            printGivenLevel(root.right, level-1);
+        }
+    }
